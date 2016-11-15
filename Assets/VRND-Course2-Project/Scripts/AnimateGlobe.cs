ab2f56917f9c8b2b2fe219539de87da5a8eb39ad
Chashmeet Singh <chashmeetsingh@gmail.com>
2016-11-15 19:19:30 +0530

    Initial Commit

---
+++ Assets/VRND-Course2-Project/Scripts/AnimateGlobe.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateGlobe : MonoBehaviour {
+
+	public float speed = 30f;
+
+	// Update is called once per frame
+	void Update () {
+		SpinGlobe ();
+	}
+
+	public void SpinGlobe () {
+		// Continuously spin our Globe
+		transform.Rotate(Vector3.up, speed * Time.deltaTime);
+	}
+
+}
